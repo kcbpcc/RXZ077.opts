58e43ff84e790b01e08488a75e44acdd4270f0c8
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-16 21:51:46 +0530

    Update cmbddfpxy.py

--- pxy/sys/exe/run/cmbddfpxy.py
+++ pxy/sys/exe/run/cmbddfpxy.py
@@ -58,7 +58,7 @@ def process_data():
         positions_df.to_csv('pxypositions.csv', index=False)
 
         if holdings_df.empty and positions_df.empty:
-            print("Both holdings and positions DataFrames are empty.")
+            print("Both holdings and positions are empty.")
             return None
 
         if not holdings_df.empty:
