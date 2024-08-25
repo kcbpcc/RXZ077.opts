89049e4eec8456b87c4bf8613521af0b5b7f2a30
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-25 12:42:43 +0530

    Update cntrloptpxy.py

--- pxy/sys/exe/run/cntrloptpxy.py
+++ pxy/sys/exe/run/cntrloptpxy.py
@@ -120,21 +120,12 @@ finally:
         sys.stdout = sys.__stdout__
 
 combined_df = process_data()
-exe_opt_df = (
-    combined_df[combined_df['key'].str.contains('NFO:', case=False)].copy()
-    if not combined_df.empty
-    else pd.DataFrame()
-)
+exe_opt_df = combined_df[combined_df['key'].str.contains('NFO:', case=False)].copy()
+exe_opt_df['key'] = exe_opt_df['key'].str.replace('NFO:', '') 
+exe_opt_df['PL%'] = (exe_opt_df['PnL'] / exe_opt_df['Invested']) * 100
+exe_opt_df['PL%'] = exe_opt_df['PL%'].fillna(0)
 
-# Assuming combined_df is already created from process_data()
-if not combined_df.empty:
-    exe_opt_df = combined_df[combined_df['key'].str.contains('NFO:', case=False)].copy()
-    exe_opt_df['key'] = exe_opt_df['key'].str.replace('NFO:', '') 
-    exe_opt_df['PL%'] = (exe_opt_df['PnL'] / exe_opt_df['Invested']) * 100
-    exe_opt_df['PL%'] = exe_opt_df['PL%'].fillna(0)
-    exe_opt_df['strike'] = exe_opt_df['key'].str.replace(r'(PE|CE)$', '', regex=True)
-
-
+exe_opt_df['strike'] = exe_opt_df['key'].str.replace(r'(PE|CE)$', '', regex=True)
 
 def compute_tgtoptsma(row):
     global bsma
