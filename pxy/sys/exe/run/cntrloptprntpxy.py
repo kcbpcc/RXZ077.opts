c038617a14bf19a8b64114c8d0839f6ab9873370
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-28 21:13:54 +0530

    Update cntrloptprntpxy.py

--- pxy/sys/exe/run/cntrloptprntpxy.py
+++ pxy/sys/exe/run/cntrloptprntpxy.py
@@ -199,15 +199,16 @@ output_lines.append(
 )
 output_lines.append(
     left_aligned_format.format(
-        f"{'A/C-tCap'.zfill(7)}:{BRIGHT_YELLOW}{str(round(CnC_tCap_rounded + (total_invested_all / 100000) + cashround), 2)).zfill(5)}"
-        f"{BRIGHT_RED if mktpredict == 'FALL' else GREY if mktpredict == 'SIDE' else BRIGHT_GREEN}    {BOLD}PXY{RESET}"
+        f"A/C-tCap: {BRIGHT_YELLOW}{str(round(CnC_tCap_rounded + (total_invested_all / 100000) + cashround, 2)).zfill(5)}{RESET}"
+        f"{BRIGHT_RED if mktpredict == 'FALL' else (GREY if mktpredict == 'SIDE' else BRIGHT_GREEN)}    {BOLD}PXY{RESET}"
     ) +
     right_aligned_format.format(
-        f"{BRIGHT_GREEN if nmktpxy in ['Bull'] else (BRIGHT_RED if nmktpxy in ['Bear'] else GREY)}"
+        f"{BRIGHT_GREEN if nmktpxy == 'Bull' else (BRIGHT_RED if nmktpxy == 'Bear' else GREY)}"
         f"{BOLD}®{RESET}{BRIGHT_YELLOW} {arrow_map.get(nmktpxy, '')}{RESET}    "
-        f"{'A/C-tPnL'.zfill(5)}:{BRIGHT_GREEN}{str(round(CnC_tCap_rounded + (total_invested_all / 100000) + cashround - 2.0, 2)).zfill(5)}{RESET}"
+        f"A/C-tPnL: {BRIGHT_GREEN}{str(round(CnC_tCap_rounded + (total_invested_all / 100000) + cashround - 2.0, 2)).zfill(5)}{RESET}"
     )
 )
+
 #output_lines.append(
     #left_aligned_format.format(f"B-Expiry:{GREY}{last_wednesday}{RESET}") +
     #right_aligned_format.format(f"N-Expiry:{GREY}{last_thursday}{RESET}")
