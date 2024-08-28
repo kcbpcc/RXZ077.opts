a7fb5ce686a7d17f029b506573d7d4e88b1180b3
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-28 21:19:14 +0530

    Update cntrloptprntpxy.py

--- pxy/sys/exe/run/cntrloptprntpxy.py
+++ pxy/sys/exe/run/cntrloptprntpxy.py
@@ -190,8 +190,7 @@ output_lines.append(left_aligned_format.format(f"F&O-tCap:{BRIGHT_YELLOW}{str(ro
         #f"Flush:{BRIGHT_GREEN if green_Stocks_capital_percentage > 0 else BRIGHT_RED}{str(round(green_Stocks_capital_percentage, 2)).zfill(4)}% {int(green_Stocks_profit_loss / 1000)}K{RESET}"))
 output_lines.append(
     left_aligned_format.format(
-
-        f"CashNow:{BRIGHT_GREEN if live_balance > 50000 else BRIGHT_YELLOW}{int(round(live_balance, 0)):06d}{RESET}"
+        f"CashNow:{BRIGHT_GREEN if cashround > 50000 else BRIGHT_YELLOW}{round(cashround, 2):08.2f}{RESET}"
     ) + 
     right_aligned_format.format(
         f"{current_month_abbr}:{month_loss_percentage}:{BRIGHT_GREEN if month_opts_total_value > 0 else BRIGHT_RED}{str(month_opts_total_value).zfill(5)}{RESET}"        
@@ -224,7 +223,6 @@ output_lines.append(
 full_output = '\n'.join(output_lines)
 
 print(full_output)
-print(cashround)
 #print("━" * 42)
 
 for group, data in grouped_df:
