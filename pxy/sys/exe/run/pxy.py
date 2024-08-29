c353f3d90c92573e3f27e4d1b0d9ef48f72ff209
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-29 07:49:00 +0530

    Update pxy.py

--- pxy/sys/exe/run/pxy.py
+++ pxy/sys/exe/run/pxy.py
@@ -203,6 +203,8 @@ while True:
     elif run_type == 'l':
         subprocess.run(['python3', 'cntrloptprntpxy.pyc', 'l'])
     print("━" * 42)
+    subprocess.run(['python3', 'misordpxy.py.pyc'])
     subprocess.run(['python3', 'selfpxy.pyc'])
+    
     progress_bar(cycle, (mktpxy if peak in ["PEAKSART", "PEAKEND", "NONPEAK"] else None))
 ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################    ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################ 
