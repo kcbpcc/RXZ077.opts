c7b7924301cf616d1670b09e9e5f6806db57ad18
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-30 23:08:18 +0530

    Update buyboptpxy.py

--- pxy/sys/exe/run/buyboptpxy.py
+++ pxy/sys/exe/run/buyboptpxy.py
@@ -164,7 +164,7 @@ async def main():
                     elif mktpxy == "Sell":
                         if bnk_power > 0.70:
                             if PE_position_exists:
-                                if PE_PLPREC < -3 and qty_PE < 3:
+                                if (PE_PLPREC < -3 and qty_PE < 2) or (PE_PLPREC < -4 and qty_PE < 3) or (PE_PLPREC < -5 and qty_PE < 4):
                                     print(f"{PE_symbol} is there,But {BRIGHT_RED}Re-Buy{RESET}")
                                     await place_order(broker, PE_symbol, 'BUY', 'MIS', 15, 'MARKET')
                                 else:
@@ -179,7 +179,7 @@ async def main():
                     if mktpxy == "Buy":
                         if bnk_power < 0.30:
                             if CE_position_exists:
-                                if CE_PLPREC < -3 and qty_CE < 3:
+                                if (CE_PLPREC < -3 and qty_CE < 2) or (CE_PLPREC < -4 and qty_CE < 3) or (CE_PLPREC < -5 and qty_CE < 4):
                                     print(f"{CE_symbol} is there,But {BRIGHT_RED}Re-Buy{RESET}")
                                     await place_order(broker, CE_symbol, 'BUY', 'NRML', 15, 'MARKET')
                                 else:
