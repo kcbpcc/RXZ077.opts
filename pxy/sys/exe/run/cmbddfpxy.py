2f6f5ed202b9b87065ba1efb3f8505eb6a0119ae
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-09-01 10:10:33 +0530

    Update cmbddfpxy.py

--- pxy/sys/exe/run/cmbddfpxy.py
+++ pxy/sys/exe/run/cmbddfpxy.py
@@ -38,11 +38,8 @@ def process_data():
     try:
         positions_response = broker.kite.positions().get('net', [])
         positions_df = get_positionsinfo(positions_response)
-        
-        if positions_df.empty:  # Check if positions_df is empty
-            print("No positions available.")
-            return pd.DataFrame()
 
+        # Removed the empty DataFrame check
         positions_df.to_csv('pxypositions.csv', index=False)
         combined_df = positions_df.reset_index(drop=True)
         combined_df['key'] = combined_df['exchange'] + ":" + combined_df['tradingsymbol']
@@ -67,3 +64,4 @@ def process_data():
 
 if __name__ == "__main__":
     process_data()
+
