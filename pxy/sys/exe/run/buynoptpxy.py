3da32c220b2cda5ccc1c666fa0d70876bedb7923
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-09-04 16:40:09 +0530

    Update buynoptpxy.py

--- pxy/sys/exe/run/buynoptpxy.py
+++ pxy/sys/exe/run/buynoptpxy.py
@@ -142,7 +142,7 @@ async def main():
                             await process_orders(broker, available_cash, False, PE_position_exists, None, PE_symbol, count_CE, count_PE, mktpxy)
                     
                     elif mktpxy == "Sell":
-                        if nse_power > 0.70:
+                        if nse_power > 0.10:
                             if CE_position_exists:
                                 if (CE_PLPREC > 3 and qty_CE < 2) or (CE_PLPREC > 5 and qty_CE < 3) or (CE_PLPREC > 7 and qty_CE < 4):
                                     print(f"    {CE_symbol} is there, {BRIGHT_YELLOW}lets check{RESET}")
