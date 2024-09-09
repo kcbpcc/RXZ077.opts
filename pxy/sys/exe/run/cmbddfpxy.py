792654bbc4a9ca3931cdecfa7db6e467b594fbe0
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-09-09 20:42:09 +0530

    Update cmbddfpxy.py

--- pxy/sys/exe/run/cmbddfpxy.py
+++ pxy/sys/exe/run/cmbddfpxy.py
@@ -6,9 +6,7 @@ from cnstpxy import dir_path
 from toolkit.logger import Logger
 import os
 import logging
-
 logger = Logger(30, dir_path + "main.log")  # Renamed logger instance
-
 def get_positionsinfo(resp_list):
     try:
         if resp_list:  # Check if the response list is not empty
@@ -20,7 +18,6 @@ def get_positionsinfo(resp_list):
     except Exception as e:
         print(f"An error occurred in positions: {e}")
         return pd.DataFrame()
-
 try:
     sys.stdout = open('output.txt', 'w')
     broker = get_kite()
@@ -38,10 +35,8 @@ def process_data():
     try:
         positions_response = broker.kite.positions().get('net', [])
         positions_df = get_positionsinfo(positions_response)
-        
         if positions_df.empty:  # Check if positions_df is empty
             return pd.DataFrame()
-
         positions_df.to_csv('pxypositions.csv', index=False)
         combined_df = positions_df.reset_index(drop=True)
         combined_df['key'] = combined_df['exchange'] + ":" + combined_df['tradingsymbol']
@@ -52,25 +47,21 @@ def process_data():
         combined_df['avg'] = combined_df.get('average_price', 0)
         combined_df['Invested'] = (combined_df['qty'] * combined_df['avg']).round(0).astype(int)
         combined_df['value'] = combined_df['qty'] * combined_df['ltp']
-
         # Ensure these columns exist or handle missing columns gracefully
         if 'day_sell_price' in combined_df.columns and 'day_sell_quantity' in combined_df.columns:
             combined_df['booked'] = (combined_df['day_sell_price'] - combined_df['average_price']) * combined_df['day_sell_quantity']
         else:
             combined_df['booked'] = 0  # Handle missing data case
-
-        combined_df['PnL'] = round(combined_df['unrealised'] - combined_df['booked'], 2)
+        combined_df['PnL'] = (combined_df['unrealised'] - combined_df['booked']).round(2).astype(int)
         combined_df['PL%'] = round((combined_df['PnL'] / combined_df['Invested'] * 100), 2)
         combined_df['Yvalue'] = combined_df['qty'] * combined_df['close']
         combined_df['dPnL'] = combined_df['value'] - combined_df['Yvalue']
         combined_df.to_csv('pxycombined.csv', index=False)
         return combined_df
-
     except Exception as e:
         print(f"An error occurred: {e}")
         traceback.print_exc()
         return None
-
 if __name__ == "__main__":
     process_data()
 
