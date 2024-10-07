552c3d5adc79ad34e175b147066bbbc9235e256d
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-10-07 09:36:47 +0530

    Update cntrloptpxy.py

--- pxy/sys/exe/run/cntrloptpxy.py
+++ pxy/sys/exe/run/cntrloptpxy.py
@@ -169,7 +169,7 @@ def compute_depth(row):
         
         if row['key'].endswith("CE") and row['key'].startswith("BANK"):
             if bcedepth > 1:
-                return max(3.9, (24- bcedepth))
+                return max(3.9, (row['repeat']- bcedepth))
             elif bpedepth > 1:
                 return 34
             else:
@@ -177,7 +177,7 @@ def compute_depth(row):
 
         elif row['key'].endswith("PE") and row['key'].startswith("BANK"):
             if bpedepth > 1:
-                return max(3.9, (24- bpedepth))
+                return max(3.9, (row['repeat']- bpedepth))
             elif bcedepth > 1:
                 return 34
             else:
@@ -185,7 +185,7 @@ def compute_depth(row):
 
         elif row['key'].endswith("CE") and row['key'].startswith("NIFTY"):
             if ncedepth > 1:
-                return max(3.9, (24- ncedepth))
+                return max(3.9, (row['repeat']- ncedepth))
             elif npedepth > 1:
                 return 34
             else:
@@ -193,7 +193,7 @@ def compute_depth(row):
 
         elif row['key'].endswith("PE") and row['key'].startswith("NIFTY"):
             if npedepth > 1:
-                return max(3.9, (24- npedepth))
+                return max(3.9, (row['repeat']- npedepth))
             elif ncedepth > 1:
                 return 34
             else:
